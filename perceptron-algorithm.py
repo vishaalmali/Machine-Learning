ba0822f7c8e4fac5f255591b89abb3c0287c1a34
Vishaal Mali <Vishaal Mali>
2017-03-29 20:43:30 -0500

    This perceptron algorithm has been trained on the Iris data set (code for training is included

--- perceptron-algorithm.py
+++ perceptron-algorithm.py
@@ -106,6 +106,7 @@ def plot_decision_regions(X, y, classifier, resolution = .02):
 	# Plot the decision surface
 	x1_min, x1_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 	x2_min, x2_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+
 	xx1, xx2 = np.meshgrid(np.arange(x1_min, x1_max, resolution), np.arange(x2_min, x2_max, resolution))
 	Z = classifier.predict(np.array([xx1.ravel(), xx2.ravel()]).T)
 	Z = Z.reshape(xx1.shape)
@@ -121,7 +122,7 @@ plot_decision_regions(X, y, classifier = ppn)
 plt.xlabel('sepal length [cm]')
 plt.ylabel('petal length [cm]')
 plt.legend(loc = 'upper left')
-plt.show()
+plt.show();
 	
 
 
